bce9e1fed3ff15ecd53d4b5d756503f51676bfc5
Julian <dieu.julian@gmail.com>
2017-11-16 16:12:47 +0100

    Feat.labels (#2)
    * init labels
    
    * enable feature labels

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,24 +3,25 @@ import React, { Component } from 'react';
 class Result extends Component {
 
   displayPrice = (price) => price/100
-
   render() {
+    const {product} = this.props
+
     return (
       <div className="in-stock">
         <h2>
-          <a href={this.props.product.url} target="blank">
-            {this.props.product.name}
+          <a href={ product.url } target="blank">
+            { product.name }
           </a>
         </h2>
         <p>
-          <b>${this.displayPrice(this.props.product.price)}</b>
+          <b>${ this.displayPrice(product.price) }</b>
         </p>
         <p>
-          {this.props.product.description}
+          { product.description }
         </p>
         <p>
           <small>
-            <i>{this.props.product.inStock ? "In Stock" : "Not available"}</i>
+            <i>{ product.inStock ? "In Stock" : "Not available" }</i>
             </small>
         </p>
         <hr/>
--- src/components/Results.js
+++ src/components/Results.js
@@ -7,7 +7,7 @@ class Results extends Component {
     super(props);
     this.state = {
       foundProducts: this.props.products,
-      ready: false
+      ready: false,
     };
   }
 
@@ -18,31 +18,62 @@ class Results extends Component {
    })
    this.setState ({
      foundProducts: foundProducts,
-     ready: true
+     ready: true,
+     all: true
    })
  }
 
+ inStock = () => {
+     this.setState ({
+     inStock: true,
+     all: false
+    })
+  }
+
+ notAvailable = () => {
+     this.setState ({
+     inStock: false,
+     all: false
+   })
+  }
+
+  allProducts = () => {
+      this.setState ({
+      all: true,
+    })
+   }
+
   render() {
-
-    const {
-      foundProducts
-    } = this.state
-
+    const {foundProducts} = this.state
     const counter = foundProducts.length
 
     return (
       <div className="results">
-        <br/>
         <div>
             <small>
               { this.state.ready && `${counter} product${counter > 1 ? 's' : ''} found` }
             </small>
+            <div className="pull-right">
+              <button className="btn btn-xs btn-default" onClick={this.allProducts}>
+                <span className="glyphicon glyphicon-list" aria-hidden="true"></span> All
+              </button>
+              <button className="btn btn-xs btn-primary" onClick={this.inStock}>
+                <span className="glyphicon glyphicon-check" aria-hidden="true"></span> In stock
+              </button>
+              <button className="btn btn-xs btn-danger" onClick={this.notAvailable}>
+                <span className="glyphicon glyphicon-remove" aria-hidden="true"></span> Not available
+              </button>
+            </div>
+            <hr/>
         </div>
         {
           foundProducts.map((product, i) => {
-            return (
-              <Result product={product} key={i} />
-            )
+            if ( product.inStock == this.state.inStock ) {
+              return <Result product={product} key={i} />
+            }
+            if ( this.state.all ) {
+              return <Result product={product} key={i} />
+            }
           })
         }
       </div>
